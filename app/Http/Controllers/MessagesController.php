56adf1586c0f15378f733d52bf1278e5eb1e2aff
tsubasa.ito <ec2-user@ip-172-31-49-189.ec2.internal>
2019-04-10 11:22:55 +0000

    add column

--- app/Http/Controllers/MessagesController.php
+++ app/Http/Controllers/MessagesController.php
@@ -49,10 +49,12 @@ class MessagesController extends Controller
     public function store(Request $request)
     {
         $this->validate($request, [
+            'title' => 'required|max:191',   // 追加
             'content' => 'required|max:191',
             ]);
             
         $message = new Message;
+        $message->title = $request->title;    // 追加
         $message->content = $request->content;
         $message->save();
         
@@ -102,10 +104,12 @@ class MessagesController extends Controller
     public function update(Request $request, $id)
     {
         $this->validate($request, [
+            'title' => 'required|max:191',   // 追加
             'content' => 'required|max:191',
         ]);
 
         $message = Message::find($id);
+        $message->title = $request->title;    // 追加
         $message->content = $request->content;
         $message->save();
 
